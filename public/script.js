ce004cc41ae0da47becce7d460126a6e7e155cc4
A Rod <aiden@as-macbook-pro.lan>
2024-02-15 20:37:29 -0500

    hi

--- public/script.js
+++ public/script.js
@@ -9,41 +9,47 @@ var peer = new Peer(undefined, {
 const user = prompt("Enter your name");
 
 const myVideo = document.createElement("video");
-myVideo.muted = true
-var myStream
-navigator.mediaDevices.getUserMedia({
-    audio:true,
-    video:true
-}).then((stream)=>{
-    myStream=stream
-    addVideoStream(myVideo, stream)
-    socket.on("user-connected", (userId)=>{
-        connectToNewUser(userId, stream)
-    })
-    peer.on("call", (call)=>{
-        call.answer(stream)
-        const video = document.createElement("video")
-        call.on("stream", (userVideoStream)=>{
-            addVideoStream(video, userVideoStream)
-        })
-    })
-})
+myVideo.muted = true;
 
-function connectToNewUser(userId, stream){
-    const call = peer.call(userId, stream)
-    const video = document.createElement("video")
-    call.on("stream", (userVideoStream)=>{
-        addVideoStream(video, userVideoStream)
-    })
-}
+let myStream;
 
-function addVideoStream(video, stream){
-    video.srcObject=stream
-    video.addEventListener("loadedmetadata", ()=>{
-        video.play()
+navigator.mediaDevices
+    .getUserMedia({
+        audio: true,
+        video: true,
+    })
+    .then((stream) => {
+        myStream = stream;
+        addVideoStream(myVideo, stream);
+
+        socket.on("user-connected", (userId) => {
+            connectToNewUser(userId, stream);
+        });
+
+        peer.on("call", (call) => {
+            call.answer(stream);
+            const video = document.createElement("video");
+            call.on("stream", (userVideoStream) => {
+                addVideoStream(video, userVideoStream);
+            });
+        });
+    })
+
+function connectToNewUser(userId, stream) {
+    const call = peer.call(userId, stream);
+    const video = document.createElement("video");
+    call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+    });
+};
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream;
+    video.addEventListener("loadedmetadata", () => {
+        video.play();
         $("#video_grid").append(video)
-    })
-}
+    });
+};
 
 $(function () {
     $("#show_chat").click(function () {
@@ -71,6 +77,36 @@ $(function () {
         }
     })
 
+    $("#mute_button").click(function () {
+        const enabled = myStream.getAudioTracks()[0].enabled;
+        if (enabled) {
+            myStream.getAudioTracks()[0].enabled = false;
+            html = `<i class="fas fa-microphone-slash"></i>`;
+            $("#mute_button").toggleClass("background_red");
+            $("#mute_button").html(html)
+        } else {
+            myStream.getAudioTracks()[0].enabled = true;
+            html = `<i class="fas fa-microphone"></i>`;
+            $("#mute_button").toggleClass("background_red");
+            $("#mute_button").html(html)
+        }
+    })
+
+    $("#stop_video").click(function () {
+        const enabled = myStream.getVideoTracks()[0].enabled;
+        if (enabled) {
+            myStream.getVideoTracks()[0].enabled = false;
+            html = `<i class="fas fa-video-slash"></i>`;
+            $("#stop_video").toggleClass("background_red");
+            $("#stop_video").html(html)
+        } else {
+            myStream.getVideoTracks()[0].enabled = true;
+            html = `<i class="fas fa-video"></i>`;
+            $("#stop_video").toggleClass("background_red");
+            $("#stop_video").html(html)
+        }
+    })
+
 })
 
 peer.on("open", (id) => {
@@ -80,7 +116,8 @@ peer.on("open", (id) => {
 socket.on("createMessage", (message, userName) => {
     $(".messages").append(`
         <div class="message">
-            <b><i class="far fa-user-circle"></i><span>${userName===user?"me":userName}</span></b>
+            <b><i class="far fa-user-circle"></i> <span> ${userName === user ? "me" : userName
+        }</span> </b>
             <span>${message}</span>
         </div>
     `)
